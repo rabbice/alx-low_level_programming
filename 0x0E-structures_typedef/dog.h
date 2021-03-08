15238aee8ae527ec4face5371d0fae8dc8fff2ca
rabbice <iankater@gmail.com>
2021-03-08 13:33:46 +0000

    added header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - information about dogs
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+
+#endif /* _DOG_H_ */
