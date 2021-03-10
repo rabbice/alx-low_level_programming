c77c2fc4c01cb9beb0e3c1d5877634c5db917a75
rabbice <iankater@gmail.com>
2021-03-10 19:45:39 +0000

    updated header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,5 +3,6 @@
 void print_name(char *name, void (*f)(char *));
 #include <stdio.h>
 void array_iterator(int *array, size_t size, void (*action)(int));
+int _putchar(char);
 int int_index(int *array, int size, int (*cmp)(int));
 #endif
